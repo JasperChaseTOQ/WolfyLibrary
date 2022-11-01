a554b63cc4ab1e7a9fce694e6626c3378214a871
JasperChaseTOQ <103849596+JasperChaseTOQ@users.noreply.github.com>
2022-10-31 20:44:35 -0400

    setting up

---
+++ src/main/java/me/wolfylibrary/slimefun/utils/items/machines/MenuUtils.java
@@ -0,0 +1,26 @@
+package me.wolfylibrary.slimefun.utils.items.machines;
+
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public final class MenuUtils {
+
+    private MenuUtils() {}
+
+    //currently plan to use
+
+    private static final ItemStack WOLFY_MACHINE_MENU_BACKGROUND = new SlimefunItemStack("JC_MACHINE_MENU_BACKGROUND", Material.LIGHT_GRAY_STAINED_GLASS_PANE, " ");
+    private static final ItemStack WOLFY_MACHINE_MENU_INPUT_BORDER_ONE = new SlimefunItemStack("JC_MACHINE_MENU_INPUT_BORDER_ONE", Material.CYAN_STAINED_GLASS_PANE, " ");
+    private static final ItemStack WOLFY_MACHINE_MENU_INPUT_BORDER_TWO = new SlimefunItemStack("JC_MACHINE_MENU_INPUT_BORDER_TWO", Material.CYAN_STAINED_GLASS_PANE, " ");
+    private static final ItemStack WOLFY_MACHINE_MENU_OUTPUT_BORDER_ONE = new SlimefunItemStack("JC_MACHINE_MENU_OUTPUT_BORDER_ONE", Material.ORANGE_STAINED_GLASS_PANE, " ");
+    private static final ItemStack WOLFY_MACHINE_MENU_OUTPUT_BORDER_TWO = new SlimefunItemStack("JC_MACHINE_MENU_OUTPUT_BORDER_TWO", Material.ORANGE_STAINED_GLASS_PANE, " ");
+
+
+    //For future plans
+
+    private static final ItemStack WOLFY_MACHINE_MENU_POWER_BUTTON = new SlimefunItemStack("JC_MACHINE_MENU_POWER_BUTTON", Material.RED_STAINED_GLASS_PANE, " ");
+    private static final ItemStack WOLFY_MACHINE_MENU_POWER_STATUS = new SlimefunItemStack("JC_MACHINE_MENU_POWER_STATUS", Material.OAK_SIGN, " ");
+    private static final ItemStack WOLFY_MACHINE_MENU_TRANSPORT_PORT = new SlimefunItemStack("JC_MACHINE_MENU_TRANSPORT_PORT", Material.GREEN_STAINED_GLASS_PANE, " ");
+
+}
