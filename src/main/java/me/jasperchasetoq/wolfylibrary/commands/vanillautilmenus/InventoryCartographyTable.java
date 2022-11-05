f5244d5b302b176688e8d45b7f8bba2714dc42b1
JasperChaseTOQ <103849596+JasperChaseTOQ@users.noreply.github.com>
2022-11-04 22:42:14 -0400

    inventory menus + item change fix

---
+++ src/main/java/me/jasperchasetoq/wolfylibrary/commands/vanillautilmenus/InventoryCartographyTable.java
@@ -0,0 +1,24 @@
+package me.jasperchasetoq.wolfylibrary.commands.vanillautilmenus;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
+public class InventoryCartographyTable implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            Inventory menutype = Bukkit.createInventory(player, InventoryType.CARTOGRAPHY);
+            player.openInventory(menutype);
+
+        }
+        return true;
+
+    }
+}
